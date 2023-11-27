1a3d6c770f81f0dd2e88f868d51b11a0d0de7029
Bart Massey <bart.massey@gmail.com>
2023-11-26 20:26:33 -0800

    greatly simplified the demo

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,27 @@ use rtt_target::{rprintln, rtt_init_print};
 use cortex_m_rt::entry;
 use microbit::{board::Board, hal::rtc};
 
-struct Ticker<T> {
-    rtc: rtc::Rtc<T>,
-    last_time: u32,
-}
+struct Ticker<T>(rtc::Rtc<T>);
 
 impl<T: rtc::Instance> Ticker<T> {
     fn new(rtc: T) -> Result<Self, rtc::Error> {
         let rtc = rtc::Rtc::new(rtc, 33)?;
-        rtc.enable_counter();
-        let last_time = rtc.get_counter();
-        Ok(Ticker { rtc, last_time })
+        rtc.disable_counter();
+        Ok(Ticker(rtc))
     }
 
-    fn wait_for_tick(&mut self) {
-        let mut now = self.rtc.get_counter();
-        // XXX Empirical constant. Math says 993.
-        while now - self.last_time < 970 {
-            now = self.rtc.get_counter();
+    fn wait_one_second(&mut self) {
+        self.0.clear_counter();
+        // XXX Must wait for counter to actually clear.
+        while self.0.get_counter() != 0 {
+            // spin
         }
-        self.last_time = now;
+        self.0.enable_counter();
+        // XXX Empirical constant. Math says 993.
+        while self.0.get_counter() < 970 {
+            // spin
+        }
+        self.0.disable_counter();
     }
 }
 
@@ -37,7 +38,7 @@ fn main() -> ! {
     let mut ticker = Ticker::new(board.RTC0).unwrap();
     for sec in 0u64.. {
         rprintln!("{}", sec);
-        ticker.wait_for_tick();
+        ticker.wait_one_second();
     }
     panic!("too many seconds");
 }
